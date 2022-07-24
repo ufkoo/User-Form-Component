87e0745c921c2a9e6d3d71c5bdd3c50845fe2faa
Ufuk <98399730+ufkoo@users.noreply.github.com>
2022-07-24 22:21:38 +0300

    Add files via upload

---
+++ API/User/User.API/Data/UsersDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Users.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.API.Data
+{
+    public class UsersDbContext: DbContext 
+    {
+
+        public UsersDbContext(DbContextOptions options) : base(options) { }
+
+        //db set
+
+        public DbSet<> Users { get; set; }
+    }
+}
---
+++ API/User/User.API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.API.Models
+{
+    public class User
+    {
+
+        [Key]
+
+        public  Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Birthdate { get; set; }
+
+        public string City { get; set; }
+    }
+}
